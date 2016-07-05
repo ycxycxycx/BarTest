c6297b1876d1f092a550c0a9d8858c89035c2b7f
HAHAYCX <ycx6100312069@gmail.com>
2016-07-05 15:08:54 +0800

    第二次更新

--- app/src/main/java/my/bar/MyListview.java
+++ app/src/main/java/my/bar/MyListview.java
@@ -4,28 +4,28 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ListView;
 
-public class MyListview extends ListView {  
-  
-    public MyListview(Context context) {  
+public class MyListview extends ListView {
+
+    public MyListview(Context context) {
         // TODO Auto-generated method stub  
-        super(context);  
-    }  
-  
-    public MyListview(Context context, AttributeSet attrs) {  
+        super(context);
+    }
+
+    public MyListview(Context context, AttributeSet attrs) {
         // TODO Auto-generated method stub  
-        super(context, attrs);  
-    }  
-  
-    public MyListview(Context context, AttributeSet attrs, int defStyle) {  
+        super(context, attrs);
+    }
+
+    public MyListview(Context context, AttributeSet attrs, int defStyle) {
         // TODO Auto-generated method stub  
-        super(context, attrs, defStyle);  
-    }  
-  
-    @Override  
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {  
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         // TODO Auto-generated method stub  
-        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,  
-                MeasureSpec.AT_MOST);  
-        super.onMeasure(widthMeasureSpec, expandSpec);  
-    }  
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
 }  
